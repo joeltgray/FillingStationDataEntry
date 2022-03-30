fa0d1eec44f0411a0f5bd7313ffcc4ffb048b440
Joel <joel@digitally.com>
2022-03-30 15:35:10 +0100

    FIX FUKN FUEL FORM

--- startFlask.py
+++ startFlask.py
@@ -93,7 +93,7 @@ def index(results={}):
     else:
         conn.close()
 
-    f = open('Paptwitter/templates/fuelForm.html','w')
+    f = open('/srv/http/pickapump.com/PapTwitter/templates/fuelForm.html','w')
     start = """<html lang="en">
                 <head>
                     <!-- Required meta tags -->
